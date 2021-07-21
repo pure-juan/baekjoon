9bc6a312560aaac3fcd4227579d5346bfb5a58c9
juan.kim <juan.kim@3i.ai>
2021-07-21 10:25:43 +0900

    Add test code

---
+++ structure/Sort/Quick/main_test.go
@@ -0,0 +1,32 @@
+/**
+* goos: darwin
+* goarch: arm64
+* pkg: github.com/pure-juan/baekjoon/structure/Sort/Quick
+* BenchmarkWithoutGoRoutine-8     2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* 2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* 2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* 2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* 2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* 1000000000               0.0000131 ns/op
+* 2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* BenchmarkGoRoutine-8            2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* 2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* 2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* 2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* 2 6 7 10 12 22 26 32 34 38 48 50 58 65 68 76 87 94 96 97
+* 1000000000               0.0000276 ns/op
+* PASS
+* ok      github.com/pure-juan/baekjoon/structure/Sort/Quick      0.157s
+ */
+
+package main
+
+import "testing"
+
+func BenchmarkWithoutGoRoutine(b *testing.B) {
+	main()
+}
+
+func BenchmarkGoRoutine(b *testing.B) {
+	main2()
+}
